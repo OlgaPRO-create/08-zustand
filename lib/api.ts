358fb9b0d3c9e317ee491260e260881de556f122
OlgaPRO-create <obrizhak2016@gmail.com>
2025-12-15 20:02:02 +0200

    08-zustand

--- lib/api.ts
+++ lib/api.ts
@@ -13,28 +13,72 @@ export interface createNotePost {
 }
 axios.defaults.baseURL = 'https://notehub-public.goit.study/api';
 
+
+
+// export default async function fetchNotes(
+//   query: string,
+//   page: number,
+//   tag?: string
+// ): Promise<NoteHttpResponse> {
+//   const response = await axios.get<NoteHttpResponse>('/notes', {
+//     params: {
+//       search: query,
+//       page,
+//       tag: tag || undefined,
+//       perPage: 8,
+//     },
+//     headers: {
+//       accept: 'application/json',
+//       Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
+//     },
+//   });
+//   return response.data;
+// }
+
+// export async function createNote({ title, content, tag }: createNotePost): Promise<Note> {
+
 export default async function fetchNotes(
   query: string,
   page: number,
   tag?: string
 ): Promise<NoteHttpResponse> {
-  const response = await axios.get<NoteHttpResponse>('/notes', {
-    params: {
-      search: query,
-      page,
-      tag: tag || undefined,
-      perPage: 8,
-    },
-    headers: {
-      accept: 'application/json',
-      Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
-    },
-  });
+  const params: {
+    page: number;
+    perPage: number;
+    search?: string;
+    tag?: string;
+  } = {
+    page,
+    perPage: 8,
+  };
+
+
+  if (query.trim() !== '') {
+    params.search = query;
+  }
+
+  if (tag && tag !== 'all') {
+    params.tag = tag;
+  }
+ 
+const response = await axios.get<NoteHttpResponse>('/notes', {
+   params,
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
+      },
+});
+  
   return response.data;
 }
 
-export async function createNote({ title, content, tag }: createNotePost): Promise<Note> {
-  const postResponse = await axios.post<Note>(
+
+export async function createNote({
+  title,
+  content,
+  tag,
+}: createNotePost): Promise<Note> {
+  const response = await axios.post<Note>(
     '/notes',
     { title, content, tag },
     {
@@ -44,24 +88,28 @@ export async function createNote({ title, content, tag }: createNotePost): Promi
       },
     }
   );
-  return postResponse.data;
+
+  return response.data;
 }
+
 export async function deleteNote(id: string): Promise<Note> {
-  const deleteResponse = await axios.delete<Note>(`/notes/${id}`, {
+  const response = await axios.delete<Note>(`/notes/${id}`, {
     headers: {
       accept: 'application/json',
       Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
     },
   });
-  return deleteResponse.data;
+  return response.data;
 }
 
 export async function fetchNoteById(id: string): Promise<Note> {
-  const responseById = await axios.get<Note>(`/notes/${id}`, {
+  const response = await axios.get<Note>(`/notes/${id}`, {
     headers: {
       accept: 'application/json',
       Authorization: `Bearer ${process.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
     },
   });
-  return responseById.data;
+  return response.data;
 }
+
+
